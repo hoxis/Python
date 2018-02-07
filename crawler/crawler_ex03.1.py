4c079f0a34e9d7645e05a90b8e064f351595f0ea
liuhao <liuhao@cmss.chinamobile.com>
2018-02-07 18:36:07 +0800

    爬虫闯关第四题-多线程解法

--- crawler/crawler_ex03.1.py
+++ crawler/crawler_ex03.1.py
@@ -2,82 +2,11 @@
 
 import requests, bs4
 import threading
-import queue
 import time
-from threading import Thread
 
-
-def getPassword(page):
-    payload['page'] = page
-    print('loading data from %s?page=%s ...' %(pwd_url, page))
-    pwd_response = requests.get(pwd_url, cookies=cookies, params=payload)
-
-    soup = bs4.BeautifulSoup(pwd_response.text, "html.parser")
-
-    pwd_pos = soup.findAll('td', {'title':'password_pos'})
-    pwd_value = soup.findAll('td', {'title':'password_val'})
-
-    for index in range(len(pwd_pos)):
-        pwd_data[int(pwd_pos[index].getText())] = pwd_value[index].getText()
-    q.put('ok')
-    print('now the pwd_data length is %s' % len(pwd_data))
-    
-
-# 通过观察，密码应该有100个数字组成。
-# 由于每次获取到的密码会有重复，所以不是一次查询完就能获取到所有数字
-# 这里一直进行查询，直到获取到100个数字
-# while len(pwd_data) < 100:
-#     # 因为每一页的密码位置都是随机给出的，其实这里可以不传page参数，一直调用pwd_url也可以获取到全部密码
-    
-#     for page in range(1,14):
-#         if q.get():
-#             t=threading.Thread(target=getPassword,args=(page,))
-#             t.start()
-#         if len(pwd_data)==100:
-#             break
-
-
-class MyThread(Thread):  
-    def __init__(self, s):  
-        Thread.__init__(self)  
-        self.s = s  
-      
-    def run(self):  
-        global count  
-        global pwdlist  
-        global exit  
-        ruler = re.compile(r'.*>(\d*)<.*')  # 提取密码位置和值的正则表达式  
-        while len(pwd_data) < 100:  
-            pwd_response = requests.get(pwd_url, cookies=cookies)
-            soup = bs4.BeautifulSoup(pwd_response.text, "html.parser")
-
-            pwd_pos = soup.findAll('td', {'title':'password_pos'})
-            pwd_value = soup.findAll('td', {'title':'password_val'})
-
-            pwdpage = s.get(pwd_website).content  
-            password_pos = BeautifulSoup(pwdpage, 'html.parser').findAll('td', {'title': 'password_pos'})  
-            password_val = BeautifulSoup(pwdpage, 'html.parser').findAll('td', {'title': 'password_val'})  
-            for index in range(len(pwd_pos)):
-                pwd_data[int(pwd_pos[index].getText())] = pwd_value[index].getText()
-            print('now the pwd_data length is %s' % len(pwd_data))
-        if exit == 0:  
-            exit = 1  
-            print ''.join(pwdlist)  
-              
-if __name__ == '__main__':  
-    start = time.time()
-    payload = {}
-    pwd_data = {}
-
-    # 题目URL
-    url = 'http://www.heibanke.com/lesson/crawler_ex03/'
-
+def login():
     # 登录URL，获取cookie
     login_url = 'http://www.heibanke.com/accounts/login/?next=/lesson/crawler_ex03/'
-
-    # 获取密码URL
-    pwd_url = 'http://www.heibanke.com/lesson/crawler_ex03/pw_list/'
-
     login_data = {'username':'liuhaha', 'password':'123456'}
 
     # 获取默认cookie
@@ -92,11 +21,61 @@ if __name__ == '__main__':
     if login_response.status_code == 200:
         print('login sucessfully')
 
+    return login_response.cookies
+
+def getPassword(page):
+    global pwd_data
+    payload['page'] = page
+    print(threading.currentThread().getName() + ', loading %s?page=%s ...' %(pwd_url, page))
+    pwd_response = requests.get(pwd_url, cookies=cookies, params=payload)
+
+    soup = bs4.BeautifulSoup(pwd_response.text, "html.parser")
+
+    pwd_pos = soup.findAll('td', {'title':'password_pos'})
+    pwd_value = soup.findAll('td', {'title':'password_val'})
+
+    for index in range(len(pwd_pos)):
+        pwd_data[int(pwd_pos[index].getText())] = pwd_value[index].getText()
+    print(threading.currentThread().getName() + ', now the pwd_data length is %s' % len(pwd_data))
+    
+class MyThread(threading.Thread):  
+    def __init__(self, s):  
+        threading.Thread.__init__(self)
+        self.s = s  
+      
+    def run(self):  
+        global pwd_data
+        global count
+        while len(pwd_data) < 100:
+            count += 1
+            print('The sub-thread has run %s times' % count)
+            getPassword(count % 13)
+              
+if __name__ == '__main__':  
+    start = time.time()
+    payload = {}
+    # 存放密码键值对
+    pwd_data = {}
+    # 记录运行次数
+    count = 0
+
+    # 题目URL
+    url = 'http://www.heibanke.com/lesson/crawler_ex03/'
+     # 获取密码URL
+    pwd_url = 'http://www.heibanke.com/lesson/crawler_ex03/pw_list/'
+   
     # 获取登录成功后的cookie
-    cookies = login_response.cookies 
-    for i in range(0, 20):  # 线程数,可自定义  
-        thread = MyThread(s)  
+    cookies = login()
+
+    threads = []
+    for i in range(0, 5):  # 线程数,可自定义  
+        thread = MyThread(cookies)
+        threads.append(thread)
         thread.start()
+    
+    # 等待所有线程完成
+    for thread in threads:
+        thread.join()
     
     # 拼接password
     password = ''
