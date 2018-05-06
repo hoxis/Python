00478923b49dc72b6a96f68ef17a8a73404b72b1
liuhao <liuhao@cmss.chinamobile.com>
2018-05-06 16:18:00 +0800

    python 包处理

---
+++ itcast/mymodule/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+
+setup(name="liuhao", version="1.0", 
+    description="liuhao's module", author="liuhao", 
+    py_modules=['suba.aa', 'suba.bb', 'subb.cc', 'subb.dd'])
